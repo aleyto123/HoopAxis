2ef102e4dbc739cf786c241a906323496093bc86
aleyto123 <rony.bellido@tecsup.edu.pe>
2026-07-18 12:13:57 -0500

    mejoramiento del dashboard

---
+++ app/src/main/java/com/tecsup/hoopaxis/data/model/RecentChapter.kt
@@ -0,0 +1,9 @@
+package com.tecsup.hoopaxis.data.model
+
+data class RecentChapter(
+    val id: Int,
+    val title: String,
+    val categoryName: String,
+    val progress: Float,
+    val iconEmoji: String
+)
